21136ad1b0237f5924169ae51ceb6aafe1ed30a2
polegar85 <124074124+polegar85@users.noreply.github.com>
2023-02-03 21:04:58 +0200

    Delete asdf.spec.js

--- asdf.spec.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
-
-test('visit page', async ({ page }) => {
-  await page.goto('https://skroutz.gr/');
-
-  // Expect a title "to contain" a substring.
-  //await expect(page).toHaveTitle(/kroutz/);
-});
