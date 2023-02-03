53c945a71b41aa0a508437658a5b280085052ef2
polegar85 <124074124+polegar85@users.noreply.github.com>
2023-02-03 20:18:49 +0200

    Add files via upload

---
+++ first_test/asdf.spec.js
@@ -0,0 +1,9 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('visit page', async ({ page }) => {
+  await page.goto('https://skroutz.gr/');
+
+  // Expect a title "to contain" a substring.
+  // await expect(page).toHaveTitle(/kroutz/);
+});
